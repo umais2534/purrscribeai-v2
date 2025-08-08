be42cedccbd0c97ad492c8acb70a2897e80cd047
Umais Iqbal <malikumais777@gmail.com>
2025-08-08 23:40:39 +0500

    pr0d-v2

--- src/config/auth0Config.ts
+++ src/config/auth0Config.ts
@@ -1,6 +1,6 @@
 export const auth0Config = {
-  domain: "dev-jtiaaa6as2dvrknq.us.auth0.com", // e.g., dev-xyz123.us.auth0.com
-  clientId: "i4w42sHzZgHE3J1x3aXP46P9tn67aCAe",
+  domain: "dev-425tsbqbsydrl817.us.auth0.com", // e.g., dev-xyz123.us.auth0.com
+  clientId: "khOeXW1zhuOR8qAz4xHbOIhtWVsMWy9y",
   redirectUri: "http://localhost:5173",
   audience: "", // optional
 };
