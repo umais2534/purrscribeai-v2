f6e40f78d12ba3e0fc4d4a59f5e9982029d76ffa
Umais Iqbal <malikumais777@gmail.com>
2025-08-09 22:22:00 +0500

    fix/prod-issue-v5

--- src/config/auth0Config.ts
+++ src/config/auth0Config.ts
@@ -1,6 +1,6 @@
 export const auth0Config = {
   domain: "dev-jtiaaa6as2dvrknq.us.auth0.com", // e.g., dev-xyz123.us.auth0.com
   clientId: "i4w42sHzZgHE3J1x3aXP46P9tn67aCAe",
-   redirectUri: import.meta.env.VITE_AUTH_REDIRECT_URI || "http://localhost:5173/dashboard",
+  redirectUri: `${window.location.origin}/dashboard`,
   audience: "", // optional
 };
